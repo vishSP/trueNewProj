d4667f9f70bb95b830d9da65c6338fe43c4fa84a
vishSP <hihi64741@gmail.com>
2023-01-20 13:57:06 +0300

    добавил 2 функцию

--- str_func.py
+++ str_func.py
@@ -1,3 +1,10 @@
 """функция делает буквы большими """
+
 def up_str(stroke):
     return stroke.upper()
+
+
+"""функция делает только первые буквы большими """
+
+def up_three(stroke):
+    return stroke.title()
